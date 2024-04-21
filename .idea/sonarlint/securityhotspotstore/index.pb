265007b0d6751d3a380214e58665c0e98e910c03
rong5026 <rong5026@naver.com>
2024-04-21 22:30:28 +0900

    :sparkles: codeStyleConfig.xml 추가

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -14,3 +14,13 @@ S
 #.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+]
+-src/main/java/animal/meeting/entity/User.java,1/e/1e3c8da8eb428593daf0f27c881355f3cba27c18
+`
+0src/main/java/animal/meeting/BaseTimeEntity.java,2/f/2f6114cbf502df9b5da91a471fca58601fe02b21
+d
+4src/main/java/animal/meeting/entity/type/Gender.java,5/9/59987af6f8e2b09b0ec3aa128fd81986d43575ab
+i
+9src/main/java/animal/meeting/entity/type/AninmalType.java,8/6/86d33317008ca596e8558c25fbd6e79ed37c4153
+T
+$.idea/codeStyles/codeStyleConfig.xml,d/d/dd7b3350c29c49497756c7f32c69a3bed18b1cba
---
+++ src/main/java/animal/meeting/BaseTimeEntity.java
@@ -0,0 +1,29 @@
+package animal.meeting;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+	//생성일자는 수정하면 안되니까 update = false
+	@CreatedDate
+	@Column(updatable=false)
+	private LocalDateTime createAt;
+
+	// 조회한 Entity 값을 변경할 때 시간이 자동 저장
+	@LastModifiedDate
+	@Column(updatable=true)
+	private LocalDateTime updatedAt;
+
+}
