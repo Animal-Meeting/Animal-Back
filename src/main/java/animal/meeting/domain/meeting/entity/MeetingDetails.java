b66bc06d16bcf61f605dacf558eb603a28b7edd9
rong5026 <rong5026@naver.com>
2024-04-23 19:48:13 +0900

    delete node_modules

--- src/main/java/animal/meeting/domain/meeting/entity/MeetingDetails.java
+++ src/main/java/animal/meeting/domain/meeting/entity/MeetingDetails.java
@@ -1,7 +1,28 @@
 package animal.meeting.domain.meeting.entity;
 
+import org.hibernate.annotations.Comment;
 
-public abstract class MeetingDetails {
+import animal.meeting.BaseTimeEntity;
+import animal.meeting.domain.meeting.entity.type.MeetingStatus;
+import animal.meeting.domain.user.entity.type.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@MappedSuperclass
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public abstract class MeetingDetails extends BaseTimeEntity {
 
+	@Comment("매칭 상태")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private MeetingStatus status;
+
+	@Comment("그룹 성별")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Gender gender;
 }
