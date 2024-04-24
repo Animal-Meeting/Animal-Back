83f920fbb3e632c8b5adf42aec89408da2f301d1
rong5026 <rong5026@naver.com>
2024-04-24 16:46:52 +0900

    :art: 생성날짜, 수정날짜 분리

--- src/main/java/animal/meeting/domain/BaseTimeEntity.java
+++ src/main/java/animal/meeting/domain/BaseTimeEntity.java
@@ -1,9 +1,8 @@
-package animal.meeting;
+package animal.meeting.domain;
 
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
@@ -16,12 +15,7 @@ import lombok.Getter;
 @Getter
 public abstract class BaseTimeEntity {
 
-	//생성일자는 수정하면 안되니까 update = false
 	@CreatedDate
-	@Column(updatable = false)
-	private LocalDateTime createAt;
-
-	// 조회한 Entity 값을 변경할 때 시간이 자동 저장
-	@LastModifiedDate
-	private LocalDateTime updatedAt;
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdAt;
 }
