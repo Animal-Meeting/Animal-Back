a04ac4df90959ea34ec85f03c0c34ce0f01642cd
rong5026 <rong5026@naver.com>
2024-04-24 16:46:26 +0900

    :art: 생성날짜, 수정날짜 분리

---
+++ src/main/java/animal/meeting/domain/meeting/entity/MatchingResult.java
@@ -0,0 +1,40 @@
+package animal.meeting.domain.meeting.entity;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.Comment;
+
+import animal.meeting.domain.BaseTimeEntity;
+import animal.meeting.domain.meeting.entity.type.MeetingGroupType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MatchingResult extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Comment("남자 그룹 ID")
+	@Column(nullable = false)
+	private UUID manGroupId;
+
+	@Comment("여자 그룹 ID")
+	@Column(nullable = false)
+	private UUID girlGroupId;
+
+	@Comment("미팅 타입")
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private MeetingGroupType meetingType;
+
+	@Comment("오픈 카카오톡 링크")
+	@Column(columnDefinition = "varchar(50)", unique = true)
+	private String kakaoLink;
+}
