f7f83aeadeff9e915e36506ca235d95507657254
rong5026 <rong5026@naver.com>
2024-04-26 20:44:00 +0900

    :sparkles: User등록시 메서드 인자 유효하지 않을 때 예외

--- src/main/java/animal/meeting/global/error/constants/CommonErrorCode.java
+++ src/main/java/animal/meeting/global/error/constants/CommonErrorCode.java
@@ -13,6 +13,7 @@ public enum CommonErrorCode implements ErrorCode {
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류, 관리자에게 문의하세요"),
 	METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "Enum Type이 일치하지 않거나 값이 없습니다."),
 	REQUESTED_PARAM_NOT_VALIDATE(HttpStatus.BAD_REQUEST, "Reqeust Param 값이 유효하지 않습니다"),
+	REQUESTED_VALUE_NOT_VALIDATE(HttpStatus.BAD_REQUEST, "메서드의 인자가 유효하지 않습니다."),
 	NOT_FOUND_REQUEST_ADDRESS(HttpStatus.NOT_FOUND, "잘못된 요청 주소입니다."),
 	NOT_FOUND_REQUEST_RESOURCE(HttpStatus.NOT_FOUND, "존재하지 않은 요청 주소입니다."),
 	;
