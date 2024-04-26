1a1170ae819c10e0b8631ee3e67eadf0f02d9e8e
rong5026 <rong5026@naver.com>
2024-04-26 20:44:15 +0900

    :sparkles: User등록시 메서드 인자 유효하지 않을 때 예외

--- src/main/java/animal/meeting/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/animal/meeting/global/error/handler/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -114,7 +115,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	/**
-	 * 요청 리소스가 없을 경우
+	 * 요청 리소스가 없을 경우 (없는 주소로 요청)
 	 */
 	@Override
 	protected ResponseEntity<Object> handleNoResourceFoundException(
@@ -143,6 +144,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		log.error("MissingServletRequestParameterException : {}", ex.getMessage(), ex);
 
 		final ErrorCode errorCode = CommonErrorCode.REQUESTED_PARAM_NOT_VALIDATE;
+		final ErrorResponse errorResponse = ErrorResponse.of(ex.getClass().getSimpleName(), errorCode.getMessage());
+		final GlobalResponse response = GlobalResponse.fail(errorCode.getHttpStatus().value(), errorResponse);
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
+	}
+
+	/**
+	 * @Valid나 @Validated 애노테이션을 사용하여 요청 본문 또는 메서드의 인자에 대한 유효성을 검사할 때 발생
+	 * List<User>의 request를 받을 때 발생
+	 */
+	@Override
+	protected ResponseEntity<Object> handleHandlerMethodValidationException(
+		HandlerMethodValidationException ex,
+		HttpHeaders headers,
+		HttpStatusCode status,
+		WebRequest request) {
+
+		log.error("HandlerMethodValidationException : {}", ex.getMessage(), ex);
+
+		final ErrorCode errorCode = CommonErrorCode.REQUESTED_VALUE_NOT_VALIDATE;
 		final ErrorResponse errorResponse = ErrorResponse.of(ex.getClass().getSimpleName(), errorCode.getMessage());
 		final GlobalResponse response = GlobalResponse.fail(errorCode.getHttpStatus().value(), errorResponse);
 		return ResponseEntity.status(errorCode.getHttpStatus()).body(response);
