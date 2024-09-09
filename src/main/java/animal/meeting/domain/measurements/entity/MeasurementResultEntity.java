45a14ffec2331e82907957e6a24d8d36753fb159
rong5026 <rong5026@naver.com>
2024-09-09 23:52:05 +0900

    :recycle: 남,여 공통필드 MeasurementResultEntity로 뺌

---
+++ src/main/java/animal/meeting/domain/measurements/entity/MeasurementResultEntity.java
@@ -0,0 +1,50 @@
+package animal.meeting.domain.measurements.entity;
+
+import animal.meeting.domain.BaseTimeEntity;
+import animal.meeting.domain.user.entity.type.AnimalType;
+import animal.meeting.domain.user.entity.type.Gender;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MeasurementResultEntity extends BaseTimeEntity {
+
+	@Column(unique = true)
+	@NotNull
+	private String studentId;
+
+	private String photoUrl;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private AnimalType animalType;
+
+	@Enumerated(EnumType.STRING)
+	@Column(columnDefinition = "enum('MALE', 'FEMALE')", nullable = false)
+	private Gender gender;
+
+	@NotNull
+	private Integer dogScore;
+
+	@NotNull
+	private Integer catScore;
+
+	@NotNull
+	private Integer rabbitScore;
+
+	protected MeasurementResultEntity(String studentId, String photoUrl, AnimalType animalType, Gender gender,
+		Integer dogScore, Integer catScore, Integer rabbitScore) {
+		this.studentId = studentId;
+		this.photoUrl = photoUrl;
+		this.animalType = animalType;
+		this.gender = gender;
+		this.dogScore = dogScore;
+		this.catScore = catScore;
+		this.rabbitScore = rabbitScore;
+	}
+}
