c201eada3c63070b553577e5c249b5409f17aaf2
rong5026 <rong5026@naver.com>
2024-09-10 17:48:17 +0900

    :sparkles: @getter 추가

--- src/main/java/animal/meeting/domain/measurements/dto/request/FemaleMeasurementResultRequest.java
+++ src/main/java/animal/meeting/domain/measurements/dto/request/FemaleMeasurementResultRequest.java
@@ -1,7 +1,9 @@
 package animal.meeting.domain.measurements.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
+@Getter
 public class FemaleMeasurementResultRequest extends CommonMeasurementRequest {
 	@NotNull(message = "여우 점수를 입력해주세요.") Integer desertFoxScore;
 	@NotNull(message = "사슴 점수를 입력해주세요.")  Integer deerScore;
