c33bf96ac81436bcfde61d6926933d374b7ae6fa
rong5026 <rong5026@naver.com>
2024-09-12 17:28:10 +0900

    :bug: docker compsoe 파일 지정

--- scripts/minipc-docker-deploy.sh
+++ scripts/minipc-docker-deploy.sh
@@ -8,7 +8,7 @@ cd /home/hong/app/animal-back
 
 # 새로운 Docker 이미지 빌드
 echo "### Docker 이미지 빌드 시작 ..." >> $DEPLOY_LOG
-docker-compose build >> $DEPLOY_LOG 2>&1
+docker-compose -f docker-compose.minipc.yml build >> $DEPLOY_LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo "Docker 이미지 빌드 실패!" >> $DEPLOY_LOG
@@ -19,7 +19,7 @@ echo "### Docker 이미지 빌드 완료!" >> $DEPLOY_LOG
 
 # 기존에 실행 중인 컨테이너 중지 및 삭제
 echo "### 기존 컨테이너 종료 및 삭제 ..." >> $DEPLOY_LOG
-docker-compose down >> $DEPLOY_LOG 2>&1
+docker-compose -f docker-compose.minipc.yml down >> $DEPLOY_LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo "기존 컨테이너 종료 및 삭제 실패!" >> $DEPLOY_LOG
@@ -28,7 +28,7 @@ fi
 
 # 새로운 컨테이너 실행
 echo "### Docker 컨테이너 실행 시작 ..." >> $DEPLOY_LOG
-docker-compose up -d >> $DEPLOY_LOG 2>&1
+docker-compose -f docker-compose.minipc.yml up -d >> $DEPLOY_LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo "Docker 컨테이너 실행 실패!" >> $DEPLOY_LOG
