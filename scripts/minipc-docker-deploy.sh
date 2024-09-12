29288e6bea648954d6c6bedab0b13fd515ddf3cd
rong5026 <rong5026@naver.com>
2024-09-12 17:32:15 +0900

    :bug: docker compsoe 이미 실행중이면 종료 후 배포

--- scripts/minipc-docker-deploy.sh
+++ scripts/minipc-docker-deploy.sh
@@ -7,32 +7,36 @@ DEPLOY_LOG="/home/hong/app/animal-back/deploy.log"
 cd /home/hong/app/animal-back
 
 # 새로운 Docker 이미지 빌드
-echo "### Docker 이미지 빌드 시작 ..." >> $DEPLOY_LOG
-docker-compose -f docker-compose.minipc.yml build >> $DEPLOY_LOG 2>&1
+echo "Docker 이미지 빌드 시작 ..." >> $DEPLOY_LOG
+docker-compose build >> $DEPLOY_LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo "Docker 이미지 빌드 실패!" >> $DEPLOY_LOG
   exit 1
 fi
 
-echo "### Docker 이미지 빌드 완료!" >> $DEPLOY_LOG
+echo "Docker 이미지 빌드 완료!" >> $DEPLOY_LOG
 
-# 기존에 실행 중인 컨테이너 중지 및 삭제
-echo "### 기존 컨테이너 종료 및 삭제 ..." >> $DEPLOY_LOG
-docker-compose -f docker-compose.minipc.yml down >> $DEPLOY_LOG 2>&1
-
-if [ $? -ne 0 ]; then
-  echo "기존 컨테이너 종료 및 삭제 실패!" >> $DEPLOY_LOG
-  exit 1
+# 기존 컨테이너 중지 및 삭제 (존재 여부 확인 후 중지/삭제)
+if [ "$(docker ps -a -q -f name=animal-meeting-container)" ]; then
+  echo "기존 컨테이너 종료 및 삭제 ..." >> $DEPLOY_LOG
+  docker stop animal-meeting-container >> $DEPLOY_LOG 2>&1
+  docker rm animal-meeting-container --force >> $DEPLOY_LOG 2>&1
+  if [ $? -ne 0 ]; then
+    echo "기존 컨테이너 종료 및 삭제 실패!" >> $DEPLOY_LOG
+    exit 1
+  fi
+else
+  echo "삭제할 기존 컨테이너가 없습니다." >> $DEPLOY_LOG
 fi
 
 # 새로운 컨테이너 실행
-echo "### Docker 컨테이너 실행 시작 ..." >> $DEPLOY_LOG
-docker-compose -f docker-compose.minipc.yml up -d >> $DEPLOY_LOG 2>&1
+echo "Docker 컨테이너 실행 시작 ..." >> $DEPLOY_LOG
+docker-compose up -d >> $DEPLOY_LOG 2>&1
 
 if [ $? -ne 0 ]; then
   echo "Docker 컨테이너 실행 실패!" >> $DEPLOY_LOG
   exit 1
 fi
 
-echo "### Docker 컨테이너 실행 완료!" >> $DEPLOY_LOG
+echo "Docker 컨테이너 실행 완료!" >> $DEPLOY_LOG
