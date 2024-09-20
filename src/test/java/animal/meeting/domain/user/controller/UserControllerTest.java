b34045ad036c817a1c9569922835a804e9fafa9b
rong5026 <rong5026@naver.com>
2024-09-20 21:56:35 +0900

    :sparkles: UserControllerTest init

---
+++ src/test/java/animal/meeting/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package animal.meeting.domain.user.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@Test
+	void registerUserAndMeeting() {
+	}
+
+	@Test
+	void getParticipantCount() {
+	}
+
+	@Test
+	void checkValidUser() {
+	}
+
+	@Test
+	void requestVarificationCode() {
+	}
+}
