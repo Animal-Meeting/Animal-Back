aac03895edcd41b10ff6a29bbe9eb9744d8d5cfa
rong5026 <rong5026@naver.com>
2024-09-20 22:46:21 +0900

    :art: Jpa Auditing 파일 따로 생성 - test 코드 jpaMappingContext 방지

--- src/main/java/animal/meeting/MeetingApplication.java
+++ src/main/java/animal/meeting/MeetingApplication.java
@@ -2,12 +2,9 @@ package animal.meeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-// @SpringBootApplication
-@EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
+@SpringBootApplication
 public class MeetingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MeetingApplication.class, args);
